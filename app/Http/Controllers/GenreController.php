88385f835a1d2d06596f18fdcfeb3d84541fb82a
Ricardo Domingues <ricardosdomingues95@gmail.com>
2021-06-06 16:52:08 +0100

    Added authentication middleware to genre controller

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -9,6 +9,17 @@ use App\Http\Resources\Genre as GenreResource;
 
 class GenreController extends Controller
 {
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     /**
      * Display a listing of the resource.
      *
